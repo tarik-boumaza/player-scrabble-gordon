194ddb563592828aa40b65da4fab5f4dd1899bc7
Tarik Boumaza <tarik.boumaza@etu.univ-lyon1.fr>
2020-03-11 16:43:01 +0100

    Bag::Bag(), tableau de 102 char

--- src/game.hpp
+++ src/game.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <list>
+#include <utility>
 
 class Board;
 class Player;
@@ -37,6 +38,9 @@ public:
   bool isFinished() const;
 
   void useLetter(const std::list<unsigned int> & l);
+
+  std::pair<unsigned short int, unsigned short int> score
+                            (const unsigned char & p, const unsigned char & l) const;
 
 
 
