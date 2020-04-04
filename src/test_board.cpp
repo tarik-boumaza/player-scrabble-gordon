057032028f4af955e7efdc894d8f3c660a16a794
hana.sebia <hana.sebia@etu.univ-lyon1.fr>
2020-04-04 16:06:43 +0200

    fonction mirror

--- src/test_board.cpp
+++ src/test_board.cpp
@@ -9,11 +9,45 @@
 
 #include <list>
 
+
+
+std::list<std::string> test(const std::string & word ){
+    std::list<std::string> l;
+    std::string temp(word);
+    std::string beginning;
+    std::string result;
+    int i,j, z;
+    std::cout<<temp[0]<<std::endl;
+    for (i = temp.size()-1 ; i > -1; i--){
+      result.clear();
+      for (j = temp.size()-1 ; j >= i; j--){
+        result = temp[j] + result;
+        std::cout<<"je sors de la première boucle : " << result <<std::endl;
+      }
+
+      result =  "+" + result;
+      //result =  temp[0] + result;
+      std::cout<<"j'ajoute le + : " << result <<std::endl;
+      z  = 0;
+      while(z > i){
+        result = temp[z] + result;
+      }
+
+      std::cout<<"j'ajoute le reste : " << beginning + result <<std::endl;
+      l.push_back(result);
+    }
+    return l;
+}
+
 int main() {
 
- 
 
-  Bag * bag = new Bag ("./data/letters.txt","./data/points.txt");
+  std::list<std::string> res = test("HANA");
+  /*while(!res.empty()){
+    std::cout<< res.front()<<std::endl;
+    res.pop_front();
+  }*/
+  /*Bag * bag = new Bag ("./data/letters.txt","./data/points.txt");
   bag->printPoints();
 
   Game g;
@@ -72,11 +106,12 @@ int main() {
   Move m ("ALPHABETIQUE",0,'B');
   unsigned short int s = g.score(m,0);
   std::cout << std::endl << "Score final : " << s << std::endl;
-  
+
 
   //g.moveTurn();
-  g.print();
-  
+  g.print();*/
+
+
 
 
 
