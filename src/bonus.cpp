33b179ccf91b6581d95390f888c7c8a27e636f95
Tarik Boumaza <tarik.boumaza@etu.univ-lyon1.fr>
2020-04-08 15:25:45 +0200

    quelques commentaires

--- src/bonus.cpp
+++ src/bonus.cpp
@@ -10,16 +10,6 @@ unsigned char Bonus::getWordFactor() const {
   return word_factor;
 }
 
-//apply the letter factor on a letter score
-unsigned int Bonus::apply_letter(unsigned int score) const {
-  return letter_factor * score ;
-}
-
-//apply the word factor on a word score
-unsigned int Bonus::apply_word(unsigned int score) const {
-  return word_factor * score ;
-}
-
 
 Bonus& Bonus::operator = (const Bonus & b){
   this->letter_factor = b.letter_factor;
--- src/bonus.hpp
+++ src/bonus.hpp
@@ -1,35 +1,59 @@
+/**
+ * @file bonus.hpp
+*/
+
 #pragma once
 
-/* Bonus */
 
+/**
+ * @class Bonus
+ * @brief Classe permettant d'attribuer des bonus à des cases du plateau
+*/
 class Bonus {
 
 public:
-  //no bonus by default, factors are 1
+  
+  /**
+   * @brief Constructeur par défaut
+   * @details Bonus par défauts : facteur 1 pour lettre et pour mot
+  */
   Bonus() :
     letter_factor(1), word_factor(1)
   {}
 
-  //initialization with provided factors
-  // * lf is the letter factor
-  // * wf is the word factor
+  /**
+   * @brief Constructeur avec initialisation des facteurs
+   * @param[in] lf : facteur lettre
+   * @param[in] wf : facteur mot
+  */
   Bonus(unsigned char lf, unsigned char wf) :
     letter_factor(lf), word_factor(wf)
   {}
 
+  /**
+   * @fn unsigned char getLetterFactor() const
+   * @brief Accesseur
+   * @details Renvoie le facteur lettre
+  */
   unsigned char getLetterFactor() const;
 
+  /**
+   * @fn unsigned char getWordFactor() const
+   * @brief Accesseur
+   * @details Renvoie le facteur mot
+  */
   unsigned char getWordFactor() const;
 
-  //apply the bonus to scores
-  unsigned int apply_letter(unsigned int score) const ;
-  unsigned int apply_word(unsigned int score) const ;
-
-  //affectation operator
+  /**
+   * @fn Bonus& operator = (const Bonus & b)
+   * @brief Operateur d'affectation
+   * @param[in] b \e Bonus
+   * @return Bonus&
+  */
   Bonus& operator = (const Bonus & b);
 
 private:
-  //stored factors
+
   unsigned char letter_factor ;
   unsigned char word_factor ;
 
